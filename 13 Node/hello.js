d0a726902ad0a972c3f076bea72eb14bd888a309
Sudhir Kesharwani <sudhir@spxpert.com>
2026-07-20 18:45:32 +0530

    NODEJS-Tutorial

---
+++ 13 Node/hello.js
@@ -0,0 +1,5 @@
+const math = require("./modules/math.js");
+const fs = require("fs");
+
+console.log("Add value is :" + math.addNumbers(5, 3));
+console.log("Subtract value is :" + math.subNumbers(5, 3));
