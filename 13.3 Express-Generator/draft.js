b99b1fdd92a40792b75001b5cb526007f80e44d6
Sudhir Kesharwani <sudhir@spxpert.com>
2026-08-10 17:29:14 +0530

    v1.0.1/general addition to the express boilerplate

--- 13.3 Express-Generator/draft.js
+++ 13.3 Express-Generator/draft.js
@@ -5,3 +5,12 @@ setTimeout(() => console.log("Hello from timer-01"));
 setImmediate(() => console.log("Hello from Immediate Function.-02"));
 
 //console.log("Hello from Top-Level");
+fs.readFile("./data.json", (err, data) => {
+  if (err) {
+    // handle error
+    console.log(err);
+    return;
+  }
+  // no errors, process data
+  console.log(data);
+});
