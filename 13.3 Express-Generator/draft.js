369d7d2222351910de44a781ac82e18f93d94b8c
Sudhir Kesharwani <sudhir@spxpert.com>
2026-08-10 16:38:22 +0530

    v1.0.0/express-generator code

---
+++ 13.3 Express-Generator/draft.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+
+setTimeout(() => console.log("Hello from timer-01"));
+
+setImmediate(() => console.log("Hello from Immediate Function.-02"));
+
+//console.log("Hello from Top-Level");
