15f72d30e02e41f0169b899eabd4e78f2da8ed91
Sudhir Kesharwani <sudhir@spxpert.com>
2026-08-12 15:30:45 +0530

    v1.0.3/Completed upload and removed node modules

--- 13.5 node-file-upload/index.js
+++ 13.5 node-file-upload/index.js
@@ -2,9 +2,18 @@
 const path = require("path");
 const express = require("express");
 const multer = require("multer"); //import multer
-const upload = multer({ dest: "./uploads" }); // folder
 
 //configure multr storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    return cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    return cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storage }); //Create multer object with storage configuration
 
 //init the app
 const app = express();
