96f4c53f0935a1290f5f7e030bc13792c894ae73
Sudhir Kesharwani <sudhir@spxpert.com>
2026-08-12 15:15:35 +0530

    v1.0.1/File upload working

--- 13.5 node-file-upload/index.js
+++ 13.5 node-file-upload/index.js
@@ -1,5 +1,11 @@
+//import required modules
 const path = require("path");
 const express = require("express");
+const multer = require("multer"); //import multer
+const upload = multer({ dest: "./uploads" }); // folder
+
+//configure multr storage
+
 //init the app
 const app = express();
 const PORT = 8000;
@@ -8,13 +14,20 @@ const PORT = 8000;
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-//middlewares
-app.use(express.json());
-
 //routes
 app.get("/", (req, res) => {
   return res.render("homepage");
 });
+//handle file upload
+app.post("/upload", upload.single("avatar"), (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+  res.redirect("/");
+});
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false })); //require to parse data other then JSON
 
 //start the server
 app.listen(PORT, () => {
